07fd03e6a02b7fc7b8bb82bcc6dbdddd0772d612
RussellShire <russell.shire@gmail.com>
2022-12-20 12:03:20 +0000

    Add different FizzBuzz solution options

--- fizzbuzz-solutions.php
+++ fizzbuzz-solutions.php
@@ -14,3 +14,23 @@ while ($counter > 0) {
   }
   $counter -= 1;
 }
+
+// For Loop
+$output = [];
+
+for ($i = 100; $i > 0; $i--) {
+    if ($i % 15 === 0) {
+      array_push($output, "FizzBuzz");
+    } elseif ($i % 5 === 0) {
+      array_push($output, "Fizz");
+    } elseif ($i % 3 === 0) {
+      array_push($output, "Buzz");
+    } else {
+      array_push($output, $i);
+    }
+  }
+
+// Printing For Loop with a foreach
+foreach ($output as $value) {
+    echo "{$value}\n";
+}
