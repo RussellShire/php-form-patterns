45146ff2c0f536abae53bb82d05d5a53c92f1119
RussellShire <russell.shire@gmail.com>
2022-12-20 12:30:07 +0000

    Add foreach shorthand and html intergration

--- foreach-syntax.php
+++ foreach-syntax.php
@@ -1,3 +1,4 @@
+<html>
 <?php
 $scores = [
   "Alice" => 99,
@@ -20,3 +21,25 @@ echo "- foreach with keys and values:\n";
 foreach ($scores as $student => $score) {
   echo "{$student} received a score of {$score}.\n";
 }
+?>
+
+<!-- Shorthand syntax examples -->
+
+<h1>Shoe Shop</h1>
+<?php
+$footwear = [
+    "sandals" => 4,
+    "sneakers" => 7,
+    "boots" => 3
+];
+?>
+
+<p>Our footwear:</p>
+<?php
+foreach ($footwear as $type => $brands): // : replaces {
+?>
+<p>We sell <?=$brands?> brands of <?=$type?></p> // <?=$foo?> // allows php to be accessed within html 
+<?php
+endforeach; // endforeach replaces }
+?>
+</html>
